896dce829ce56feeb89cd8a45f68d725be20d5d6
3230visuals <133800761+3230visuals@users.noreply.github.com>
2026-03-08 19:32:53 -0500

    chore: upgrade to strict ESLint type checking

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,7 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.strictTypeChecked,
       tseslint.configs.stylisticTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
